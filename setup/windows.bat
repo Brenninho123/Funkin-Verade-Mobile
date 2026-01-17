c8335b8c06d967305bb3ae4be4233ad7ef77f1f7
BerGP <101405920+BernardoGP4504@users.noreply.github.com>
2026-01-17 17:28:31 -0300

    Atualizando dependências haxelib em windows.bat (fazendo actions funcionar - pt FINAL)

--- setup/windows.bat
+++ setup/windows.bat
@@ -4,16 +4,14 @@ mkdir .haxelib && haxelib setup .haxelib
 
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime
-haxelib install openfl
-haxelib git flixel https://github.com/HaxeFlixel/flixel --skip-dependencies
-haxelib install flixel-addons --skip-dependencies
+haxelib install lime 8.2.2
+haxelib install openfl 9.4.1
+haxelib install flixel 6.1.0 --skip-dependencies
+haxelib install flixel-addons 3.3.2 --skip-dependencies
 haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
 haxelib install hxdiscord_rpc 1.2.4
 haxelib install hxvlc 2.0.1 --skip-dependencies
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
-haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate b8970d2e4d875c743abe28e356a15413b1774d94
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit daf0664aee99aa8b8d6bdd1eda993a1c3a399067
 pause
